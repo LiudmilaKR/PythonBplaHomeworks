65f278b6277ea3b31cfce4982e98339d285ae63e
Liudmila <lidiay@yandex.ru>
2024-06-29 23:47:08 +0300

    homework2 - comments were added

--- task2_mod1_hw.py
+++ task2_mod1_hw.py
@@ -14,6 +14,13 @@ import random
 class Drone:
     
     def __init__(self, id='123F56', altitude=0, coords=(0.0, 0.0)) -> None:
+        """_summary_ Конструктор объекта класса Drone
+
+        Args:
+            id (str, optional): _description_. Defaults to '123F56'. id объекта класса Drone
+            altitude (int, optional): _description_. Defaults to 0. высота полёта объекта класса Drone
+            coords (tuple, optional): _description_. Defaults to (0.0, 0.0). координаты объекта класса Drone
+        """
         self.id = id
         self.altitude = altitude
         self.coordinates = coords
@@ -25,20 +32,17 @@ class Drone:
         print('Создан экземпляр класса Drone')
     
     def drone_runways(self, rise_altitude):
-        """_summary_ метод взлета БПЛА
+        """_summary_ метод взлета объекта класса Drone
 
         Args:
-            altitude (_type_): _description_ высота взлета БПЛА
-
-        Returns:
-            _type_: _description_ высота взлета
+            altitude (_type_): _description_ высота взлета объекта класса Drone
         """
         self.altitude = self.flight_controller.runways(rise_altitude)
         self.ar_altitudes.append(self.altitude)
         print(f'БПЛА взлетает на высоту {self.altitude} метров')
     
     def change_altitude(self, new_altitude: int):
-        """_summary_ метод изменения высоты БПЛА
+        """_summary_ метод изменения высоты объекта класса Drone
 
         Args:
             altitude (int): _description_ новая высота
@@ -51,58 +55,113 @@ class Drone:
         self.ar_altitudes.append(self.altitude)
     
     def change_coords(self, new_coords: tuple):
-        """_summary_ метод изменения координат БПЛА
+        """_summary_ метод изменения координат объекта класса Drone
 
         Args:
-            new_coords (tuple): _description_ новые координаты БПЛА
+            new_coords (tuple): _description_ новые координаты объекта класса Drone
         """
         self.coordinates = self.gps.update_coord(new_coords)
         self.flight_path.append(self.coordinates)
         print(f'Новые координаты БЛПА {self.coordinates}')
     
     def drone_landing(self):
-        """_summary_ метод посадки БПЛА
+        """_summary_ метод посадки объекта класса Drone
 
         """
         print(self.flight_controller.landing())
         print(f'Отчет о полёте БПЛА id={self.id}:\nКоординаты полёта {self.flight_path}\nВысоты {self.ar_altitudes}')
     
     def __str__(self) -> str:
+        """_summary_ метод отображения информации об объекте клаcса Drone
+
+        Returns:
+            str: _description_ информация об объекте класса Drone
+        """
         return f'БПЛА {self.id} с камерой: {self.camera}, начальные координаты {self.gps.coordinates}'
+    
 
 class Camera:
     
     def __init__(self, model='CD06', resolution='640x510') -> None:
+        """_summary_ конструктор объекта класса Camera
+
+        Args:
+            model (str, optional): _description_. Defaults to 'CD06'. модель объекта класса Camera
+            resolution (str, optional): _description_. Defaults to '640x510'. разрешение объекта класса Camera
+        """
         self.model = model
         self.resolution = resolution
-        # print('Создан экземпляр класса Camera')
         
     def __str__(self) -> str:
+        """_summary_ метод отображения информации об объекте клаcса Camera
+
+        Returns:
+            str: _description_ информация об объекте класса Camera
+        """
         return f'модель {self.model} разрешение {self.resolution} пикселей'
 
 
 class GPS:
     
     def __init__(self, init_coords = (0.0, 0.0)) -> None:
+        """_summary_ конструктор объекта класса GPS
+
+        Args:
+            init_coords (tuple, optional): _description_. Defaults to (0.0, 0.0). начальные координаты
+        """
         self.coordinates = init_coords
-        # print('Создан экземпляр класса GPS')
     
     def update_coord(self, coords: tuple):
+        """_summary_ метод обновления координат
+
+        Args:
+            coords (tuple): _description_ координаты
+
+        Returns:
+            _type_: _description_ кортеж координат
+        """
         self.coordinates = coords
         return self.coordinates
 
 class FlightController:
         
     def runways(self, rise_altitude: int):
+        """_summary_ метод управления взлетом
+
+        Args:
+            rise_altitude (int): _description_ высота взлета
+
+        Raises:
+            ValueError: _description_ 
+
+        Returns:
+            _type_: _description_ высота взлета
+        """
         if rise_altitude > 0:
             return rise_altitude
         else:
             raise ValueError('Высота взлета должна быть > 0')
         
     def landing(self):
+        """_summary_ метод управления приземлением
+
+        Returns:
+            _type_: _description_ сооббщение о приземлении
+        """
         return 'БПЛА приземлился'
         
     def change_altitude(self, new_altitude: int):
+        """_summary_ метод изменения высоты полёта
+
+        Args:
+            new_altitude (int): _description_ новая высота полёта
+
+        Raises:
+            ValueError: _description_
+
+        Returns:
+            _type_: _description_ высота полета
+        """
         if new_altitude > 0:
             return new_altitude
         else:
