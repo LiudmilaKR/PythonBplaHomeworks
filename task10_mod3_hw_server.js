0b6fadf24cbbbadd147ecbfcab2bce892e20db2d
LiudmilaKR <lidiay@yandex.ru>
2024-08-16 16:32:04 +0300

    module-3 task-10 addition

--- task10_mod3_hw_server.js
+++ task10_mod3_hw_server.js
@@ -5,6 +5,8 @@ const PORT = 3001;
 app.get("/time", (req, res) => {
     const currentTime = new Date();
     console.log(currentTime);
+    console.log(currentTime.getDate());
+    console.log(currentTime.getFullYear());
     res.json(currentTime)
 });
 
